442021f4f90d66159389b62224491cbd7576b31a
Monsterboii11 <bmonster819@gmail.com>
2025-12-11 12:22:40 +0545

    login and sign up pages logic done

--- quiz-game/src/auth/ProtectedRoutes.jsx
+++ quiz-game/src/auth/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext"
 
-const ProtectedRoutes = () => {
+const ProtectedRoutes = ({children}) => {
     const {user} = useAuth();
 
     if(!user) return <Navigate to='/login' />
+    return children;
 }
 
 export default ProtectedRoutes
--- quiz-game/src/pages/Signup.jsx
+++ quiz-game/src/pages/Signup.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../auth/AuthContext'
 
 const Signup = () => {
+
+    const {signup} = useAuth();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        signup(username, password);
+
+        navigate('/login')
+    }
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-            <div className="w-full max-w-sm bg-white shadow-md rounded-lg p-6">
+            <form onSubmit={handleSubmit}>
+                <div className="w-full max-w-sm bg-white shadow-md rounded-lg p-6">
 
                 <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
 
@@ -13,14 +29,8 @@ const Signup = () => {
                         type="text"
                         placeholder="Enter your username"
                         className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                </div>
-
-                <div className="mb-4">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
                     />
                 </div>
 
@@ -29,6 +39,8 @@ const Signup = () => {
                         type="password"
                         placeholder="Password"
                         className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
                     />
                 </div>
 
@@ -44,6 +56,7 @@ const Signup = () => {
                 </p>
 
             </div>
+            </form>
         </div>
     )
 }
